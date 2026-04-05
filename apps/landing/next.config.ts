47860e7a4e26e7186383f10532d36fce4e24d023
3uba <75246355+3uba@users.noreply.github.com>
2026-04-06 00:54:39 +0200

    chore: poprawki

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: "../..",
   },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/:path*",
+          has: [{ type: "host", value: "app.mitshe.com" }],
+          destination: "/coming-soon",
+        },
+      ],
+      afterFiles: [],
+      fallback: [],
+    };
+  },
 };
 
 export default nextConfig;
